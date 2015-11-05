d7e6dcb66ab3c0881a127b55f79ee0f8ebf72c89
Patric Vormstein <patric@vormstein.de>
2015-11-06 00:27:10 +0100

    build layout

--- RocketLeagueReplayJsonizer/MainWindow.xaml.cs
+++ RocketLeagueReplayJsonizer/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MahApps.Metro.Controls;
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +23,40 @@ namespace RocketLeagueReplayJsonizer {
     public partial class MainWindow : MetroWindow {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void btnFileDlg_Click(object sender, RoutedEventArgs e) {
+            lblSaved.Content = "";
+            Microsoft.Win32.OpenFileDialog fileDlg = new Microsoft.Win32.OpenFileDialog();
+
+            fileDlg.InitialDirectory = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"My Games\Rocket League\TAGame\Demos\");
+            fileDlg.Filter = "RL-Replay Files (*.replay)|*.replay";
+            if(fileDlg.ShowDialog() == true) {
+                txtBoxFileName.Text = System.IO.Path.GetFileName(fileDlg.FileName);
+                lblCreatedAt.Content = File.GetLastWriteTime(fileDlg.FileName).ToString();
+
+            }
+
+
+
+        }
+
+        private void btnSaveFile_Click(object sender, RoutedEventArgs e) {
+            lblSaved.Content = "";
+            Microsoft.Win32.SaveFileDialog saveFileDlg = new Microsoft.Win32.SaveFileDialog();
+
+            saveFileDlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Recent);
+            saveFileDlg.Filter = "JSON-File (*.json)|*.json";
+
+            if(saveFileDlg.ShowDialog() == true) {
+                File.WriteAllText(saveFileDlg.FileName, txtBoxJSON.Text);
+                lblSaved.Content = "Saved!";
+            }
+        }
+
+        private void btnCopyToClipboard_Click(object sender, RoutedEventArgs e) {
+            Clipboard.SetText(txtBoxJSON.Text);
+            lblSaved.Content = "Copied!";
         }
     }
 }
