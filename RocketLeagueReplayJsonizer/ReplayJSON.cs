b4b3b6b7e5160e9491cb062581c7c18818eb7f72
Patric Vormstein <patric@vormstein.de>
2015-11-07 01:45:33 +0100

    added JSON class

---
+++ RocketLeagueReplayJsonizer/ReplayJSON.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketLeagueReplayJsonizer {
+    class ReplayJSON {
+        const string _version = "RLRJv1";
+
+        public string ID { get; private set; }
+        public string MatchType { get; private set; }
+        public string MatchDate { get; private set; }
+        public string MatchLength { get; private set; }
+        public string Stadium { get; private set; }
+        public int TeamSize { get; private set; }
+        public List<Player> MembersTeamBlue { get; private set; }
+        public List<Player> MembersTeamOrange { get; private set; }
+        public int ScoreTeamBlue { get; private set; }
+        public int ScoreTeamOrange { get; private set; }
+        public List<Goal> Goals { get; private set; }
+    }
+
+    class Player {
+        public string Name { get; private set; }
+        public string Platform { get; private set; }
+        public string OnlineID { get; private set; }
+        public int Score { get; private set; }
+        public int Goals { get; private set; }
+        public int Assists { get; private set; }
+        public int Saves { get; private set; }
+        public int Shots { get; private set; }
+    }
+
+    class Goal {
+        public string Time { get; private set; }
+        public string Scorer { get; private set; }
+    }
+}
